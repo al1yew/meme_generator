793044c08b9599d1e992fceb52eaf4d30a8057ed
al1yew <vasifja@code.edu.az>
2022-10-23 16:48:08 +0400

    Commit

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -38,56 +38,60 @@ export default function Meme() {
 
     return (
         <main>
-            <p className="text-center firstp">Get a random meme image and write your own text!</p>
-            <div className="memeform" >
-                <input
-                    type="text"
-                    placeholder="Top text"
-                    className="col-lg-5-8 col-5-8"
-                    value={meme.topText}
-                    onChange={handleChange}
-                    name="topText"
-                />
-                <input
-                    type="text"
-                    placeholder="Bottom text"
-                    className="col-lg-5-8 col-5-8"
-                    value={meme.bottomText}
-                    onChange={handleChange}
-                    name="bottomText"
-                />
-                <input
-                    type="text"
-                    placeholder="Left text"
-                    className="col-lg-5-8 col-5-8"
-                    value={meme.leftText}
-                    onChange={handleChange}
-                    name="leftText"
-                />
-                <input
-                    type="text"
-                    placeholder="Right text"
-                    className="col-lg-5-8 col-5-8"
-                    value={meme.rightText}
-                    onChange={handleChange}
-                    name="rightText"
-                />
-                <button
-                    type="button"
-                    className="col-12 col-lg-12"
-                    onClick={handleClick}
-                >
-                    Create meme!
-                </button>
-            </div>
+            <div className="container">
+                <div className="row all">
+                    <p className="text-center firstp">Get a random meme image and write your own text!</p>
+                    <div className="memeform" >
+                        <input
+                            type="text"
+                            placeholder="Top text"
+                            className="col-lg-5-8 col-5-8"
+                            value={meme.topText}
+                            onChange={handleChange}
+                            name="topText"
+                        />
+                        <input
+                            type="text"
+                            placeholder="Bottom text"
+                            className="col-lg-5-8 col-5-8"
+                            value={meme.bottomText}
+                            onChange={handleChange}
+                            name="bottomText"
+                        />
+                        <input
+                            type="text"
+                            placeholder="Left text"
+                            className="col-lg-5-8 col-5-8"
+                            value={meme.leftText}
+                            onChange={handleChange}
+                            name="leftText"
+                        />
+                        <input
+                            type="text"
+                            placeholder="Right text"
+                            className="col-lg-5-8 col-5-8"
+                            value={meme.rightText}
+                            onChange={handleChange}
+                            name="rightText"
+                        />
+                        <button
+                            type="button"
+                            className="col-12 col-lg-12"
+                            onClick={handleClick}
+                        >
+                            Create meme!
+                        </button>
+                    </div>
 
-            <div className="memecontainer">
-                <div className="imgcont">
-                    <img src={meme.url} />
-                    <span className="toptext">{meme.topText}</span>
-                    <span className="lefttext">{meme.leftText}</span>
-                    <span className="righttext">{meme.rightText}</span>
-                    <span className="bottomtext">{meme.bottomText}</span>
+                    <div className="memecontainer">
+                        <div className="imgcont">
+                            <img src={meme.url} />
+                            <span className="toptext">{meme.topText}</span>
+                            <span className="lefttext">{meme.leftText}</span>
+                            <span className="righttext">{meme.rightText}</span>
+                            <span className="bottomtext">{meme.bottomText}</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
